71be3c891312c4070ae3011cdcdae80eb82f6d8e
nsadili <nuraddinsadili93@gmail.com>
2022-12-01 10:21:08 +0400

    Updates

---
+++ precache-manifest.28d0271a6be27814665a2ac70cea1d4f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab17032ad2cad7a01da7",
+    "url": "/react-gh-pages/static/js/main.ab17032a.chunk.js"
+  },
+  {
+    "revision": "14fcefedd3940c15128b",
+    "url": "/react-gh-pages/static/js/1.14fcefed.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "f7da7d44b01fac2247a9206abf6897d0",
+    "url": "/react-gh-pages/index.html"
+  }
+];
